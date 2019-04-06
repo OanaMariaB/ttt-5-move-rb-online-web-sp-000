683b048966f2c375f89492e749e82c32365ec290
Oana Berentan <oana.berentan@yahoo.com>
2019-04-06 14:42:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,12 +7,9 @@ def display_board(board)
 
 
   def input_to_index(user_input)
-    new_user_input = user_input.to_i
-    new_user_input -= 1
-    return new_user_input
-  end
+  user_input.to_i - 1
+end
 
-  def move(board, index, character = "X")
-    board[index] = character
-    return board
-  end
+def move(board, index, current_player)
+  board[index] = current_player
+end
