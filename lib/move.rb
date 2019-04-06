84c2123d10786a83271b834efed8ee5e3e016d80
Oana Berentan <oana.berentan@yahoo.com>
2019-04-06 14:22:52 +0000

    Automatically backed up by Learn

--- lib/move.rb
+++ lib/move.rb
@@ -7,5 +7,5 @@ def display_board(board)
 
 
 def input_to_index(user_input = ["1", "2", "3", "4", "5", "6", "7", "8", "9"])
-
+puts "1#{board[0]}, 2#{board[1]}, 3#{board[2]}, 4#{board[3]}, 5#{board[4]}, 6#{board[5]}, 7#{board[6]}, 8#{board[7]}, 9#{board[8]}"
 end
